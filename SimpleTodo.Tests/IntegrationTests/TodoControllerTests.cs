8e8bd2a4dd2801186eac712c0b5d48e0e7367846
christ.fomekong <fomekongchristmael@gmail.com>
2025-05-18 10:53:31 +0100

    Configure todo service dependencies

--- SimpleTodo.Tests/IntegrationTests/TodoControllerTests.cs
+++ SimpleTodo.Tests/IntegrationTests/TodoControllerTests.cs
@@ -1,6 +1,24 @@
+using System.Net.Http.Json;
+using Microsoft.AspNetCore.Mvc.Testing;
+using SimpleTodo.Api.Models;
+using SimpleTodo.Tests.Helpers;
+
 namespace SimpleTodo.Tests.IntegrationTests;
 
-public class TodoControllerTests
+public class TodoControllerTests(WebApplicationFactory<Program> factory) : IClassFixture<WebApplicationFactory<Program>>
 {
-    
+    private readonly HttpClient _client = factory.CreateClient();
+
+    [Fact]
+    public async Task PostTodoItem_ReturnCreatedItem()
+    {
+        await AuthenticationHelpers.Authenticate(_client, "mael0", "mael");
+        // Arrange
+        var todo = new TodoItem() { Title = "Test Task", IsCompleted = false };
+        var response = await _client.PostAsJsonAsync("/api/todo", todo);
+        response.EnsureSuccessStatusCode();
+        var todoResponse = await response.Content.ReadFromJsonAsync<TodoItem>();
+        Assert.Equal(todo.Title, todoResponse?.Title);
+        Assert.Equal(todo.IsCompleted, todoResponse?.IsCompleted);
+    }
 }
